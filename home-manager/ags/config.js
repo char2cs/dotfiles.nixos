12c957c09c9207053755b15fda392ecdc30d2017
Mateo Urrutia <mateourru@gmail.com>
2024-09-03 09:20:44 -0300

    Tst

--- home-manager/ags/config.js
+++ home-manager/ags/config.js
@@ -1,46 +1,20 @@
-import GLib from "gi://GLib"
+App.addIcons(`${App.configDir}/assets`);
+import { Bar } from './bar/bar.js';
+import { NotificationPopups } from './notifications/notificationPopups.js';
+import { setupQuickSettings } from './quicksettings/quicksettings.js';
+import { applauncher } from "./widgets/applauncher.js";
 
-const main = "/tmp/asztal/main.js"
-const entry = `${App.configDir}/main.ts`
-const bundler = GLib.getenv("AGS_BUNDLER") || "bun"
+setupQuickSettings();
+App.config({
+    windows: [
+        // this is where window definitions will go
+        // Bar(0),
+        Bar(0),
+        applauncher,
+       	// NotificationPopups(0),
+        NotificationPopups(0)
+    ],
+    style: './style.css',
+})
 
-const v = {
-    ags: pkg.version?.split(".").map(Number) || [],
-    expect: [1, 8, 1],
-}
-
-try {
-    switch (bundler) {
-        case "bun": await Utils.execAsync([
-            "bun", "build", entry,
-            "--outfile", main,
-            "--external", "resource://*",
-            "--external", "gi://*",
-            "--external", "file://*",
-        ]); break
-
-        case "esbuild": await Utils.execAsync([
-            "esbuild", "--bundle", entry,
-            "--format=esm",
-            `--outfile=${main}`,
-            "--external:resource://*",
-            "--external:gi://*",
-            "--external:file://*",
-        ]); break
-
-        default:
-            throw `"${bundler}" is not a valid bundler`
-    }
-
-    if (v.ags[1] < v.expect[1] || v.ags[2] < v.expect[2]) {
-        print(`my config needs at least v${v.expect.join(".")}, yours is v${v.ags.join(".")}`)
-        App.quit()
-    }
-
-    await import(`file://${main}`)
-} catch (error) {
-    console.error(error)
-    App.quit()
-}
-
-export { }
+export {}
